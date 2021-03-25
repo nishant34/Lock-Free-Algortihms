01c4ad263dcad5aa3e55165f64cad188d7d8a9ea
nishant jain <njain@cs.iitr.ac.in>
2021-03-25 05:54:16 +0530

    added comments
    The error with compare and exchange function still persists.

--- queue.cpp
+++ queue.cpp
@@ -1,165 +1,150 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
+#include <atomic>
+#include <iostream>
 
+template <class T> class Node {
+  // Node class representing a node of a linked list consiisting of a value data
+  // field and a pointer to the next node.
+public:
+  T value;
+  Node *next;
 
-class Node{
-    public:
-    int value;
-    Node* next;
-    
-    Node(int value){
-        this->value = value;
-        this->next = NULL;
-    }
-
+  // Constructor of the Node Class. Initializing the next pointer to Null.
+  Node(T value) {
+    this->value = value;
+    this->next = NULL;
+  }
 };
 
-
-bool cas(atomic<Node*> &mem, Node* cmp, Node* with)
-{
-    //return __sync_val_compare_and_swap(mem, cmp, with);
-    return mem.compare_exchange_strong(cmp, with,  memory_order_release, memory_order_relaxed);
-    
-    //non atomic cass
-    // if(mem==cmp){
-    //     mem = with;
-    //     // cout<<mem->value;
-    //     return true;
-    // }
-    
-    // return false;
-
+template <class T>
+bool compare_and_exchange(std::atomic<Node<T> *> &mem, Node<T> *cmp,
+                          Node<T> *with) {
+  // atomic compare and exchange function using the inbuilt
+  // compare_exchage_strong function in atomic class in C++
+  return mem.compare_exchange_strong(cmp, with, std::memory_order_release,
+                                     std::memory_order_relaxed);
 }
 
+template <class T> class Lock_Free_Queue {
+  // class to implement a lock free queue.
+  // It has to pointers similar to a standard queue: A head and a tail both of
+  // which are defined to be atomic Nodes so that they can call the atomic
+  // compare_exchange function.
+public:
+  std::atomic<Node<T> *> head;
+  std::atomic<Node<T> *> tail;
 
-class Lock_Free_Queue{
+  // A default constructor
+  Lock_Free_Queue();
+  // Initializing the Lock free queue with a given sentinel using an initializer
+  // list
+  Lock_Free_Queue(Node<T> *sentinel)
+      : head(sentinel), tail(sentinel)
 
-    
-    
-    public:
-    atomic<Node*> head;
-    atomic<Node*> tail;
-    // Lock_Free_Queue(Node* sentinel){
-        
-    //      head = sentinel;
-    //      tail = sentinel;
-    //      //cas(head, NULL, sentinel);
-    // }
-    //~Lock_Free_Queue();
-    Lock_Free_Queue();
-    Lock_Free_Queue(Node* sentinel) :head(sentinel), tail(sentinel)
-    
-    {}
-    
-    void enqueue(int value){
-        cout<<"enque"<<endl;
-        Node* t = new Node(value);
-        
-        while(true){
-        
-            Node* last = tail;
-            Node* next = last->next;
-            
-            if(last == tail){
-                if(next == NULL){
-                  
-                   //if(cas(last->next,NULL,t))
-                  // {cas(tail, last, t);
-                   //cout<<last->value<<endl;
-                   //return;}
-                    Node* curr_node = head.load(memory_order_relaxed);
-                    Node* t_1 = curr_node->next;
-                    //cout<<t_1->value;
-                    while(t_1!=NULL){
-                    t_1 = t_1->next;   
-                    }                
-                   cas(tail, last, t);
-                   return;
-                    
-                }else{
-            
-                    cas(tail, last, next);
-                }  
-            }  
-        
+  {}
+
+  void enqueue(T value) {
+    // initializing  the Node to be added
+    Node<T> *t = new Node<T>(value);
+
+    // Unless the current node is finally able to append itself it should keep
+    // on trying. The loop will continue untill Nodes other than the current are
+    // appended to the queue. As soon as the current value is appended the
+    // function called returns.
+    while (true) {
+      // Define a pointer to the tail node in the current fucntion
+      Node<T> *last = tail;
+      Node<T> *next = last->next;
+
+      // if no other Node appended itself during this time.
+      if (last == tail) {
+        // if this is not the case then the tail pointer at the wrong node.
+        if (next == NULL) {
+          // atomically adding the node to be appended to the linked list
+          if (compare_and_exchange(last->next, NULL, t)) {
+            // updating the tail pointer and as the node is appended so function
+            // returns
+            compare_and_exchange(tail, last, t);
+
+            return;
+          }
+
+        } else {
+          // As the tail pointer was not at the correct node so it needs to be
+          // updated. comparing the tail pointer with the last to check if no
+          // new node is appended and then updating it to the actual last
+          // position
+          compare_and_exchange(tail, last, next);
         }
-        Node* curr_node = head.load(memory_order_relaxed);
-        Node* t_1 = curr_node->next;
-         cout<<t_1->value; 
-         while(t_1!=NULL){
-         cout<<"Loudu"<<endl;
-         cout<<t_1->value<<" ";
-         t_1 = t_1->next;
-     }
-        
-    }   
-    
-    
-    int deque(){
-     
-        while(true){
-            cout<<"Dequeeee"<<endl;
-            Node* first = head;
-            Node* next = first->next;
-            Node* last = tail;
-            if(first == last){
-                if(next == NULL)
-                
-                throw underflow_error("called deque on empty queue");
-                //cout<<"called deque on empty queue";
-                
-                else{
-                    cas(tail, last, next);
-                }
-            }else{
-                int value = first->value;
-                
-                if(cas(head, first, next)){
-                    return value;
-                }        
-            }
-        }
-        return 1;
-        
+      }
     }
+  }
 
+  T deque() {
+
+    // looping untill the node is dequed
+    while (true) {
+      Node<T> *first = head;
+      Node<T> *next = first->next;
+      Node<T> *last = tail;
+
+      // Checking the empty queue case
+      if (first == last) {
+        // the case for the empty queue
+        if (next == NULL)
+
+          throw std::underflow_error("called deque on an empty queue");
+
+        else {
+          // tail pointer was incorrectly placed.
+          compare_and_exchange(tail, last, next);
+        }
+      } else {
+        // the value to be dequed will be returned by the fucntion
+        T value = first->value;
+
+        // if head is same as first i.e. no new node appended then just update
+        // the head pointer and return.
+        if (compare_and_exchange(head, first, next)) {
+          return value;
+        }
+      }
+    }
+    return 1;
+  }
 };
-void printQueue(Lock_Free_Queue q){
-    Node* curr_node = q.head.load(memory_order_relaxed);
-    cout<<curr_node->value;
-     Node* t = curr_node->next;
-     //t = t->next;
-    while(t!=NULL){
 
-         cout<<t->value<<" ";
-         t = t->next;
-     }
-    // if(t!=NULL){
-     //cout<<t->value<<" "<<endl;
-     //}
+template <class T> void printQueue(Lock_Free_Queue<T> q) {
+  // function to print a queue instance of the previously defined class.
+
+  // putting the current value of atomic head to the curr_node
+  Node<T> *curr_node = q.head.load(std::memory_order_relaxed);
+  std::cout << curr_node->value;
+  Node<T> *t = curr_node->next;
+
+  while (t != NULL) {
+
+    std::cout << t->value << " ";
+    t = t->next;
+  }
 }
 
-int main(){
-    
-    Node* sentinel = new Node(-1);
-    Lock_Free_Queue q(sentinel);
-    
-q.enqueue(10);
-printQueue(q);
-q.enqueue(20);
-printQueue(q);
-q.enqueue(30);
-printQueue(q);
-q.deque();
-printQueue(q);
-q.enqueue(30);
-printQueue(q);
-q.enqueue(30);
+int main() {
 
-q.deque();
+  // defining a sentinel node for the queue and iniitializing a lock free queue
+  Node<int> *sentinel = new Node<int>(-1);
+  Lock_Free_Queue<int> q(sentinel);
 
-
-
-    
+  // testing the queue
+  q.enqueue(10);
+  printQueue(q);
+  q.enqueue(20);
+  printQueue(q);
+  q.enqueue(30);
+  printQueue(q);
+  q.deque();
+  printQueue(q);
+  q.enqueue(30);
+  printQueue(q);
+  q.enqueue(30);
+  q.deque();
 }
