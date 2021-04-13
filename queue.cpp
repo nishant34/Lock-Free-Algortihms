49f0bd046c5daef2099f7aa56100201565104c94
Yugantar777 <yarya@cs.iitr.ac.in>
2021-04-14 00:16:42 +0530

    Updated code

--- queue.cpp
+++ queue.cpp
@@ -1,5 +1,7 @@
 #include <atomic>
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 template <class T> class Node {
   // Node class representing a node of a linked list consiisting of a value data
@@ -7,33 +9,15 @@ template <class T> class Node {
 public:
   T value;
   Node *next;
+  std::atomic<Node<T> *> next1;
 
   // Constructor of the Node Class. Initializing the next pointer to Null.
   Node(T value) {
     this->value = value;
+    // this->next1;
     this->next = NULL;
   }
 };
-template <class T>
-bool compare_and_exchange(std::atomic<Node<T> *> &mem, Node<T> *cmp,
-                          Node<T> *with) {
-  // atomic compare and exchange function using the inbuilt
-  // compare_exchage_strong function in atomic class in C++
-  return mem.compare_exchange_strong(cmp, with, std::memory_order_release,
-                                     std::memory_order_relaxed);
-}
-template <class T>
-bool compare_and_exchange_new(Node<T> **mem, Node<T> *cmp, Node<T> *with) {
-  // atomic compare and exchange function using the inbuilt
-  // compare_exchage_strong function in atomic class in C++
-  std::atomic<Node<T> *> temp(*mem);
-  if (temp.compare_exchange_strong(cmp, with, std::memory_order_release,
-                                   std::memory_order_relaxed)) {
-    *mem = temp.load(std::memory_order_relaxed);
-    return true;
-  }
-  return false;
-}
 
 template <class T> class Lock_Free_Queue {
   // class to implement a lock free queue.
@@ -43,6 +27,7 @@ template <class T> class Lock_Free_Queue {
 public:
   std::atomic<Node<T> *> head;
   std::atomic<Node<T> *> tail;
+  Node<T> *p;
 
   // A default constructor
 
@@ -50,13 +35,14 @@ public:
   // Initializing the Lock free queue with a given sentinel using an initializer
   // list
   Lock_Free_Queue(Node<T> *sentinel)
-      : head(sentinel), tail(sentinel)
+      : head(sentinel), tail(sentinel), p(sentinel)
 
   {}
 
-  void enqueue(T value) {
+  inline void  enqueue(T value) {
     // initializing  the Node to be added
-    Node<T> *t = new Node<T>(value);
+    Node<T> *temp = new Node<T>(value);
+    std::atomic<Node<T> *> t(temp);
 
     // Unless the current node is finally able to append itself it should keep
     // on trying. The loop will continue untill Nodes other than the current are
@@ -64,32 +50,37 @@ public:
     // function called returns.
     while (true) {
       // Define a pointer to the tail node in the current fucntion
-      Node<T> *last = tail;
-      Node<T> *next = last->next;
-
+      std::atomic<Node<T> *> last(tail.load(std::memory_order_relaxed));
+      // std::cout<<"1"<<"\n";
       // if no other Node appended itself during this time.
       if (last == tail) {
+        std::cout<<"2"<<"\n";
         // if this is not the case then the tail pointer at the wrong node.
-        if (next == NULL) {
+        if (last.load(std::memory_order_relaxed)
+                ->next1.load(std::memory_order_relaxed) == NULL) {
           // atomically adding the node to be appended to the linked list
-          if (compare_and_exchange_new<T>(&last->next, NULL, t)) {
+          std::cout<<"3"<<"\n";
+          if (last.load(std::memory_order_relaxed)
+                  ->next1.compare_exchange_strong(temp->next, t,
+                                                  std::memory_order_release,
+                                                  std::memory_order_relaxed)) {
             // comparing head with tail to check the base case which is enquing
             // the first node in our queue. So head->next needs to be updated.
+            std::cout<<"4"<<"\n";
             if (tail == head) {
-              compare_and_exchange(tail, last, t);
-              compare_and_exchange_new<T>(
-                  &head.load(std::memory_order_relaxed)->next, NULL, last);
+              Node<T> *a = last.load(std::memory_order_relaxed);
+              tail.compare_exchange_strong(a, t.load(std::memory_order_relaxed),
+                                           std::memory_order_release,
+                                           std::memory_order_relaxed);  
+              head.compare_exchange_strong(p, tail, std::memory_order_release,
+                                           std::memory_order_relaxed);
             }
             // updating the tail pointer and as the node is appended so function
             // returns
-            compare_and_exchange(tail, last, t);
-            // printng the first(node after the sentinel node) value and tail
-            // value after each enqueue operation.
-            // just for testing purpose, will be removed later.
-            std::cout << head.load(std::memory_order_relaxed)->next->value
-                      << " ";
-            std::cout << tail.load(std::memory_order_relaxed)->value
-                      << std::endl;
+            Node<T> *b = last.load(std::memory_order_relaxed);
+            tail.compare_exchange_strong(b, t.load(std::memory_order_relaxed),
+                                         std::memory_order_release,
+                                         std::memory_order_relaxed);
 
             return;
           }
@@ -99,38 +90,66 @@ public:
           // updated. comparing the tail pointer with the last to check if no
           // new node is appended and then updating it to the actual last
           // position
-          compare_and_exchange(tail, last, next);
+          std::cout<<"5"<<"\n";
+          Node<T> *c = last.load(std::memory_order_relaxed);
+          tail.compare_exchange_strong(
+              c,
+              last.load(std::memory_order_relaxed)
+                  ->next1.load(std::memory_order_relaxed),
+              std::memory_order_release, std::memory_order_relaxed);
         }
       }
     }
   }
 
-  T deque() {
+  inline T deque() {
 
     // looping untill the node is dequed
     while (true) {
-      Node<T> *first = head;
-      Node<T> *next = first->next;
-      Node<T> *last = tail;
-
+      std::atomic<Node<T> *> first(head.load(std::memory_order_relaxed));
+      std::atomic<Node<T> *> next(first.load(std::memory_order_relaxed)->next);
+      std::atomic<Node<T> *> last(tail.load(std::memory_order_relaxed));
+      Node<T> *p1;
       // Checking the empty queue case
       if (first == last) {
         // the case for the empty queue
-        if (next == NULL)
-
+        if (first == p)
           throw std::underflow_error("called deque on an empty queue");
+        else if (next.load(std::memory_order_relaxed) == NULL) {
+          Node<T> *a = head.load(std::memory_order_relaxed);
+          head.compare_exchange_strong(a, p, std::memory_order_release,
+                                       std::memory_order_relaxed);
+          Node<T> *b = tail.load(std::memory_order_relaxed);
+          tail.compare_exchange_strong(b, p, std::memory_order_release,
+                                       std::memory_order_relaxed);
 
-        else {
+          tail.load(std::memory_order_relaxed)
+              ->next1.compare_exchange_strong(a, NULL,
+                                              std::memory_order_release,
+                                              std::memory_order_relaxed);
+          return first.load(std::memory_order_relaxed)->value;
+        } else {
           // tail pointer was incorrectly placed.
-          compare_and_exchange(tail, last, next);
+          Node<T> *a = last.load(std::memory_order_relaxed);
+          tail.compare_exchange_strong(
+              a,
+              head.load(std::memory_order_relaxed)
+                  ->next1.load(std::memory_order_relaxed),
+              std::memory_order_release, std::memory_order_relaxed);
         }
       } else {
         // the value to be dequed will be returned by the fucntion
-        T value = first->value;
+        T value = first.load(std::memory_order_relaxed)->value;
 
         // if head is same as first i.e. no new node appended then just update
         // the head pointer and return.
-        if (compare_and_exchange(head, first, next)) {
+        Node<T> *b = first.load(std::memory_order_relaxed);
+
+        if (head.compare_exchange_strong(
+                b,
+                head.load(std::memory_order_relaxed)
+                    ->next1.load(std::memory_order_relaxed),
+                std::memory_order_release, std::memory_order_relaxed)) {
           return value;
         }
       }
@@ -138,16 +157,49 @@ public:
     return 1;
   }
 };
+
+void printQ(Lock_Free_Queue<int> &q) {
+
+  Node<int> *t = q.head.load(std::memory_order_relaxed);
+  while (t != NULL) {
+    std::cout << t->value << " ";
+    t = t->next1.load(std::memory_order_relaxed);
+  }
+  std::cout << "\n";
+}
+
+Node<int> *sentinal = new Node<int>(-1);
+Lock_Free_Queue<int> q(sentinal);
+
+void my(int i){
+  q.enqueue(i);
+}
+
+void my2(int i){
+  int a = q.deque();
+}
+
 int main() {
 
   // defining a sentinel node for the queue and iniitializing a lock free queue
-  Node<int> *sentinal = new Node<int>(-1);
-  Lock_Free_Queue<int> q(sentinal);
+  
 
   // testing the queue
-  q.enqueue(10);
-  q.enqueue(20);
-  q.enqueue(30);
+  // q.enqueue(10);
+  // printQ(q);
+  // q.enqueue(20);
+  // q.deque();
+  // printQ(q);
 
-   
+  std::vector<int> v;
+  for(int i = 0; i < 1000; i++){
+    v.push_back(i);
+  } 
+
+  for_each(v.begin(), v.end(), my);
+  // printQ(q);
+  // for_each(v.begin() + 100, v.end(), my2);
+  // printQ(q);
+
+ 
 }
